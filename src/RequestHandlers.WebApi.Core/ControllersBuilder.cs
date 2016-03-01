99d7572b8882d37785ae5f5b5375051b656c210e
Glenn Dierckx <glenn@windstyle.be>
2016-03-01 21:47:21 +0100

    remove saving of assembly

--- src/RequestHandlers.WebApi.Core/ControllersBuilder.cs
+++ src/RequestHandlers.WebApi.Core/ControllersBuilder.cs
@@ -9,16 +9,15 @@ namespace RequestHandlers.WebApi.Core
         public Assembly CreateControllers(Type webApiRequestProcessor, RequestHandlerDefinition[] requestHandlers, WebApiTypes webApiTypes)
         {
             var assemblyName = new AssemblyName("Generated");
-            var moduleName = string.Format("{0}.dll", assemblyName.Name);
+            var moduleName = $"{assemblyName.Name}.dll";
 
-            var assembly = AppDomain.CurrentDomain.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.RunAndSave, "c:\\");
+            var assembly = AppDomain.CurrentDomain.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.Run);
             var moduleBuilder = assembly.DefineDynamicModule(moduleName, assemblyName.Name + ".dll", true);
             var controllerBuilder = new ControllerBuilder(moduleBuilder, webApiRequestProcessor, webApiTypes);
             foreach (var requestHandlerDefinition in requestHandlers)
             {
                 controllerBuilder.CreateController(requestHandlerDefinition);
             }
-            assembly.Save(moduleName);
             return moduleBuilder.Assembly;
         }
     }
