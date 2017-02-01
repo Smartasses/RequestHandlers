7f1df9b8f9ead5dbfc85af582cb303f3d725bd07
Glenn Dierckx <glenn@windstyle.be>
2017-02-01 09:19:17 +0100

    Add process async

--- src/RequestHandlers/RequestDispatcherExtensions.cs
+++ src/RequestHandlers/RequestDispatcherExtensions.cs
@@ -1,5 +1,6 @@
 using System.Linq;
 using System.Reflection;
+using System.Threading.Tasks;
 
 namespace RequestHandlers
 {
@@ -15,6 +16,12 @@ namespace RequestHandlers
         {
             return (TResponse)MainMethod
                 .MakeGenericMethod(request.GetType(), typeof(TResponse))
+                .Invoke(dispatcher, new object[] { request });
+        }
+        public static async Task<TResponse> ProcessAsync<TResponse>(this IRequestDispatcher dispatcher, IReturn<TResponse> request)
+        {
+            return await (Task<TResponse>)MainMethod
+                .MakeGenericMethod(request.GetType(), typeof(Task<TResponse>))
                 .Invoke(dispatcher, new object[] { request });
         }
     }
